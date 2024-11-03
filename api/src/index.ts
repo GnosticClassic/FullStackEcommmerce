ac637668f2524e6743ed918b6ba95f4462afd122
Zedamat <bahrezedamat@gmail.com>
2024-11-04 01:02:47 +0300

    Json Parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
-import productsRoutes from './routes/products/index'
+import express, { json, urlencoded } from "express";
+import productsRoutes from "./routes/products/index";
 
 const app = express();
 const port = 3000;
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 app.get("/", (req, res) => {
   res.send("Hello World! 12345");
 });
-
 
 //app use routes
 app.use("/products", productsRoutes);
 app.listen(port, () => {
   console.log(`Device is listining at port ${port}`);
 });
-
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -9,6 +9,7 @@ export function getProductById(req: Request, res: Response) {
   res.send("get Product By Id ");
 }
 export function createProduct(req: Request, res: Response) {
+  console.log(req.body)
   res.send("createProduct");
 }
 export function updateProduct(req: Request, res: Response) {
